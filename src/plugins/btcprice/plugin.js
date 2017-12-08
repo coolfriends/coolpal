95ba5e8b51a758e5b5f52a3e4fa430b313f2e303
JordanRushing <rushing.jordan@gmail.com>
2017-12-08 07:09:52 +0000

    Inline guard clauses and consolidate message components for bitcoin

--- src/plugins/btcprice/plugin.js
+++ src/plugins/btcprice/plugin.js
@@ -5,25 +5,19 @@ class BitcoinPlugin {
   constructor(config = {}) {
     this.command = 'bitcoin';
     this.supported_event_types = ['message'];
-    this.url = 'https://api.coinbase.com/v2/prices/BTC-USD/buy';
+    this.url = config.url || 'https://api.coinbase.com/v2/prices/BTC-USD/buy';
     this.axios = config.axios || axios.create();
   }
 
   handle_event(event_type, event, config) {
-    if (event_type == 'message') {
-      return this.handle_message(event, config);
-    }
+    if (event_type == 'message') return this.handle_message(event, config);
     return false;
   }
 
   handle_message(message, config) {
     let command_args = utils.split_message(message);
-    if (command_args[0] != config.prefix + 'bitcoin') {
-      return false;
-    }
-    if (message.author.username == config.client.user.username) {
-      return true;
-    }
+    if (command_args[0] != config.prefix + 'bitcoin') return false;
+    if (message.author.username == config.client.user.username) return true;
     let url = this.url;
     this.call_bitcoin_api(message, url, config);
     return true;
@@ -34,8 +28,7 @@ class BitcoinPlugin {
       method: 'get',
       url: url
     }).then(response => {
-      message.reply("\nFrom Coinbase | " +
-        "Current Bitcoin price in USD: $" +
+      message.reply("\nFrom Coinbase | Current Bitcoin price in USD: $" +
         response.data.data.amount);
       return true;
     }).catch(error => {
