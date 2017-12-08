87cb574829a91f34ab3be945e332d1e09358d5ac
JordanRushing <rushing.jordan@gmail.com>
2017-12-08 06:54:26 +0000

    Fix typo in plugin_config and remove extra paren in test

--- test/test_weather.js
+++ test/test_weather.js
@@ -64,144 +64,139 @@ describe('WeatherPlugin', function() {
       });
     });
   });
-});
-describe('#handle_event()', function() {
-  it('should return true if message if well formatted', function() {
-    let message_fixture = {
-      content: '!weather denton',
-      author: {
-        username: 'notabot'
-      },
-      reply: () => {}
-    };
-    assert(plugin.handle_event('message', message_fixture, config_fixture));
+  describe('#handle_event()', function() {
+    it('should return true if message if well formatted', function() {
+      let message_fixture = {
+        content: '!weather denton',
+        author: {
+          username: 'notabot'
+        },
+        reply: () => {}
+      };
+      assert(plugin.handle_event('message', message_fixture, config_fixture));
+    });
+    it('should return false if event type is not message', function() {
+      assert(!plugin.handle_event('not_a_message', {}, {}));
+    });
   });
-  it('should return false if event type is not message', function() {
-    assert(!plugin.handle_event('not_a_message', {}, {}));
+  describe('#handle_message()', function() {
+    it('should return false if the first argument is not a match', function() {
+      let message_fixture = {
+        content: '!notweather'
+      };
+      assert(!plugin.handle_message(message_fixture, {}));
+    });
+    it('should return true if message is from the bot', function() {
+      let message_fixture = {
+        content: '!weather',
+        author: {
+          username: 'abot'
+        }
+      };
+      assert(plugin.handle_message(message_fixture, config_fixture));
+    });
+    it('should not reply if message is from the bot', function() {
+      let recorded_message = '';
+      let message_fixture = {
+        content: '!weather',
+        author: {
+          username: 'abot'
+        },
+        reply: (message) => {
+          recorded_message = message;
+        }
+      };
+      assert.equal(recorded_message, '');
+    });
+    it('should return true if city is not supported', function() {
+      let message_fixture = {
+        content: '!weather not_a_supported_city',
+        author: {
+          username: 'notabot'
+        },
+        reply: () => {}
+      };
+
+      assert(plugin.handle_message(message_fixture, config_fixture));
+    });
+    it('should reply with an informative message when unsupported city provided', function() {
+      let plugin = new WeatherPlugin;
+      plugin.city_ids = {
+        example_city: 'some_fake_id'
+      };
+
+      let recorded_message = '';
+      let message_fixture = {
+        content: '!weather this_city_is_not_supported',
+        author: {
+          username: 'notabot'
+        },
+        reply: (message) => {
+          recorded_message = message;
+        }
+      };
+      let expected = 'City: This_city_is_not_supported is not supported at this time. Try:\n' +
+        '!weather example_city\n';
+
+      plugin.handle_message(message_fixture, config_fixture);
+      assert.equal(recorded_message, expected);
+    });
+    it('should return true if message is correct', function() {
+      let message_fixture = {
+        content: '!weather denton',
+        author: {
+          username: 'notabot'
+        },
+        reply: () => {}
+      };
+
+      assert(plugin.handle_message(message_fixture, config_fixture));
+    });
+
+    it('should reply with weather data when message is correct', function() {
+      let response = {
+        data: {
+          cod: 200,
+          main: {
+            temp: '70'
+          },
+          weather: [{
+            description: 'sunny'
+          }],
+          wind: {
+            speed: '10'
+          }
+        }
+      };
+      let axios_mock = build_axios_mock(response);
+      let plugin = new WeatherPlugin({
+        openweather_api_key: 'not-a-real-api-key',
+        axios: axios_mock
+      });
+      // Make sure that the string passed to message.reply is captured.
+      let recorded_message = '';
+      let message_fixture = {
+        content: '!weather denton',
+        author: {
+          username: 'notabot'
+        },
+        reply: (message) => {
+          recorded_message = message;
+        }
+      };
+      let expected = "\nTemp: " +
+        response.data.main.temp +
+        " **|** Weather: " +
+        response.data.weather[0].description +
+        " **|** Wind: " +
+        response.data.wind.speed;
+
+      plugin.handle_message(message_fixture, config_fixture);
+
+      // wait 1 seconds to make sure the message gets recorded
+      setTimeout(() => {
+        assert.equal(recorded_message, expected);
+      }, 1);
+    });
   });
-});
-describe('#handle_message()', function() {
-it('should return false if the first argument is not a match', function() {
-  let message_fixture = {
-    content: '!notweather'
-  };
-  assert(!plugin.handle_message(message_fixture, {}));
-});
-it('should return true if message is from the bot', function() {
-  let message_fixture = {
-    content: '!weather',
-    author: {
-      username: 'abot'
-    }
-  };
-  assert(plugin.handle_message(message_fixture, config_fixture));
-});
-it('should not reply if message is from the bot', function() {
-  let recorded_message = '';
-  let message_fixture = {
-    content: '!weather',
-    author: {
-      username: 'abot'
-    },
-    reply: (message) => {
-      recorded_message = message;
-    }
-  };
-  assert.equal(recorded_message, '');
-});
-it('should return true if city is not supported', function() {
-  let message_fixture = {
-    content: '!weather not_a_supported_city',
-    author: {
-      username: 'notabot'
-    },
-    reply: () => {}
-  };
-
-  assert(plugin.handle_message(message_fixture, config_fixture));
-});
-it('should reply with an informative message when unsupported city provided', function() {
-  let plugin = new WeatherPlugin;
-  plugin.city_ids = {
-    example_city: 'some_fake_id'
-  };
-
-  let recorded_message = '';
-  let message_fixture = {
-    content: '!weather this_city_is_not_supported',
-    author: {
-      username: 'notabot'
-    },
-    reply: (message) => {
-      recorded_message = message;
-    }
-  };
-  let expected = 'City: This_city_is_not_supported is not supported at this time. Try:\n' +
-    '!weather example_city\n';
-
-  plugin.handle_message(message_fixture, config_fixture);
-  assert.equal(recorded_message, expected);
-});
-it('should return true if message is correct', function() {
-  let message_fixture = {
-    content: '!weather denton',
-    author: {
-      username: 'notabot'
-    },
-    reply: () => {}
-  };
-
-  assert(plugin.handle_message(message_fixture, config_fixture));
-});
-
-it('should reply with weather data when message is correct', function() {
-  let response = {
-    data: {
-      cod: 200,
-      main: {
-        temp: '70'
-      },
-      weather: [{
-        description: 'sunny'
-      }],
-      wind: {
-        speed: '10'
-      }
-    }
-  };
-
-  let axios_mock = build_axios_mock(response);
-  let plugin = new WeatherPlugin({
-    openweather_api_key: 'not-a-real-api-key',
-    axios: axios_mock
-  });
-
-
-  // Make sure that the string passed to message.reply is captured.
-  let recorded_message = '';
-  let message_fixture = {
-    content: '!weather denton',
-    author: {
-      username: 'notabot'
-    },
-    reply: (message) => {
-      recorded_message = message;
-    }
-  };
-
-  let expected = "\nTemp: " +
-    response.data.main.temp +
-    " **|** Weather: " +
-    response.data.weather[0].description +
-    " **|** Wind: " +
-    response.data.wind.speed;
-
-  plugin.handle_message(message_fixture, config_fixture);
-
-  // wait 1 seconds to make sure the message gets recorded
-  setTimeout(() => {
-    assert.equal(recorded_message, expected);
-  }, 1);
-});
-});
 });
