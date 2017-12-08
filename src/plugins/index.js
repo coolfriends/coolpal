ab0f57698f72c3f88545790f60d0cae887fe540e
JordanRushing <rushing.jordan@gmail.com>
2017-12-08 06:15:31 +0000

    Create bitcoin plugin and add to live bot

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,10 @@
 let name_to_class = {
   spam: require('./spam/plugin.js'),
   helloworld: require('./hello_world/plugin.js'),
-  weather: require('./weather/plugin.js')
+  weather: require('./weather/plugin.js'),
+  bitcoin: require('./btcprice/plugin.js')
 };
 
 module.exports = {
   name_to_class
-}
+};
