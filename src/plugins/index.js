ccffaeabb4c95b7a3de78883e617fe05ab69e286
JordanRushing <rushing.jordan@gmail.com>
2017-12-10 02:00:41 +0000

    Create ethereum plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,7 +2,8 @@ let name_to_class = {
   spam: require('./spam/plugin.js'),
   helloworld: require('./hello_world/plugin.js'),
   weather: require('./weather/plugin.js'),
-  bitcoin: require('./btcprice/plugin.js')
+  bitcoin: require('./btcprice/plugin.js'),
+  ethereum: require('./ethprice/plugin.js')
 };
 
 module.exports = {
