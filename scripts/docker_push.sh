05f27babff0673b28db52118bdbfe82e3f2f595e
Danny Kraft <danny.l.kraft@gmail.com>
2018-02-22 22:57:53 -0600

    Modify docker push script to correct tag and push syntax

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -10,8 +10,8 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         echo "Building $IMAGE_NAME"
         docker build -t $IMAGE_NAME .
         echo "Pushing $IMAGE_NAME:latest"
-        docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-        docker push "$REMOTE_IMAGE_URL:latest"
+        docker tag $IMAGE_NAME:latest $REMOTE_IMAGE_URL:latest
+        docker push $REMOTE_IMAGE_URL:latest
         echo "Successfully pushed $IMAGE_NAME:latest"
     else
         echo "Skipping deploy, branch not master"
