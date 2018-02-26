17bc9b09fc767fc52e40f62fdbee6321582a3a9b
kbougy <kyriayxvanderpoel@gmail.com>
2018-02-26 01:33:09 -0600

    Remove ethprice plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,7 +3,6 @@ let name_to_class = {
   'helloworld': require('./hello_world/plugin.js'),
   'weather': require('./weather/plugin.js'),
   'coinbase': require('./coinbase/plugin.js'),
-  'ethereum': require('./ethprice/plugin.js'),
   'feature-request': require('./feature-request/plugin.js'),
   'google-news': require('./google-news/plugin.js')
 };
